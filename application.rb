5f6cfd2cbfc3349b289c589ffe2c97d52eb22b99
Christoph Helma <helma@in-silico.de>
2010-03-07 12:11:28 +0100

    mask color changed

--- application.rb
+++ application.rb
@@ -24,10 +24,11 @@ Shoes.app  do
 				image @@display.image.file, :width => @@display.img_width, :height => @@display.img_height 
 				case @@keyboard.mode
 				when 'Crop'
-					rect :top => @@display.crop_bottom, :left => 0, :width => @@display.img_width, :height => @@display.img_height-@@display.crop_bottom, :fill => black#, :stroke => white
-					rect :top => 0, :left => @@display.crop_right, :width => @@display.img_width-@@display.crop_right, :height => @@display.img_height, :fill => black#, :stroke => white
-					rect :top => 0, :left => 0, :width => @@display.img_width, :height => @@display.crop_top, :fill => black#, :stroke => white
-					rect :top => 0, :left => 0, :width => @@display.crop_left, :height => @@display.img_height, :fill => black#, :stroke => white
+					fill_color = '#555'
+					rect :top => @@display.crop_bottom, :left => 0, :width => @@display.img_width, :height => @@display.img_height-@@display.crop_bottom, :fill => fill_color, :stroke => fill_color
+					rect :top => 0, :left => @@display.crop_right, :width => @@display.img_width-@@display.crop_right, :height => @@display.img_height, :fill => fill_color, :stroke => fill_color
+					rect :top => 0, :left => 0, :width => @@display.img_width, :height => @@display.crop_top, :fill => fill_color, :stroke => fill_color
+					rect :top => 0, :left => 0, :width => @@display.crop_left, :height => @@display.img_height, :fill => fill_color, :stroke => fill_color
 					para @@keyboard.mode, :size => 10, :stroke => white
 				when 'Command'
 					para "> " + @@keyboard.input, :size => 10, :stroke => white

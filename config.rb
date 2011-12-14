5c2284c01da95a4fdca280bc66b0c701753aa1ec
Christoph Helma <helma@in-silico.ch>
2011-12-14 13:38:53 +0100

    index datastructure refactored

--- config.rb
+++ config.rb
@@ -1,8 +1,17 @@
+require 'yajl'
+require 'yaml'
+require 'mini_exiftool'
+
 @dir = File.join ENV['HOME'], "images"
 @imagedir = File.join @dir, "images"
 @thumbdir = File.join @dir, "thumbs"
-@flickrdir = File.join @dir, "flickr"
 
 @indexfile = File.join @dir, "index.yaml"
 @tagfile = File.join @dir, "tag"
 @currentfile = File.join @dir, "current"
+@q_tags = ["0","1","2","3","4"]
+
+@index = YAML.load_file(@indexfile)
+@tag = File.read(@tagfile).chomp.to_s
+#@tag == "*" ? @index[@tag] = @index.values.flatten.sort : @index[@tag] = @index[@tag]
+@current = File.read(@currentfile).chomp.to_i
